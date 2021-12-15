d0bf423b8af0c3e53171313152e449e6dfe48c50
sdemchyshyn <sergas123456@gmail.com>
2021-12-15 19:50:06 +0200

    update

--- test/dev/app/js/scripts.js
+++ test/dev/app/js/scripts.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	loadFonts();
 	correctVh();
 	lazyLoad();
+	mobileMenu();
 });
 
 window.addEventListener('resize', () => {
@@ -92,3 +93,17 @@ function correctVh() {
 	let vh = window.innerHeight * 0.01;
 	document.documentElement.style.setProperty('--vh', vh + 'px');
 }
+
+// mobile menu
+function mobileMenu() {
+	const openBtn = document.querySelector('.open-menu');
+
+	openBtn.addEventListener('click', function (event) {
+		event.preventDefault();
+		if (document.body.classList.contains('menu-opened')) {
+			document.body.classList.remove('menu-opened');
+		} else {
+			document.body.classList.add('menu-opened');
+		}
+	});
+};
