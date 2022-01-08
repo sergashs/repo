f9353ca9860425e5ea104acc10ab479984188b2f
sergashs <sergas123456@gmail.com>
2022-01-08 23:45:32 +0200

    add blog

---
+++ myBlog/src/components/Header/Header.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="header">
+		<div class="container-fluid">
+			<strong class="logo">
+				<a href="#">architectural</a>
+			</strong>
+			<nav></nav>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Header",
+	props: {
+		// msg: String
+	}
+};
+</script>
+
+<style lang="scss">
+// @import "@/components/Header/Header.scss";
+
+.header {
+	.logo {
+		// font-family: $syncopate;
+		font-weight: 700;
+		letter-spacing: 0.04rem;
+	}
+}
+</style>
