daf7cd7198deeb74a8333d9d257934f3840ab15d
sergashs <sergas123456@gmail.com>
2022-03-10 17:38:15 +0200

    update

--- nuxt-deploy-blog/_nuxt/static/1646925543/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Frepo\u002Fnuxt-deploy-blog\u002F_nuxt\u002Fstatic\u002F1646925543",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Frepo\u002Fnuxt-deploy-blog\u002F",assetsPath:"\u002Frepo\u002Fnuxt-deploy-blog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
