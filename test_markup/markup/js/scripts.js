1b323530e2379c6d7e851561fab119661458f7cd
sergashs <sergas123456@gmail.com>
2022-10-12 15:52:44 +0300

    start

---
+++ test_markup/markup/js/scripts.js
@@ -0,0 +1,125 @@
+let loadLazyLoadScript = false;
+
+document.addEventListener('DOMContentLoaded', () => {
+	loadFonts();
+	correctVh();
+	lazyLoad();
+});
+
+window.addEventListener('resize', () => {
+	correctVh();
+});
+
+// load fonts
+function loadFonts() {
+	WebFont.load({
+		custom: {
+			families: ['Degular:n1,n3,n7', 'Font Awesome 5 Brands'],
+			urls: ['css/fonts.css']
+		},
+		google: {
+			families: ['Playfair Display:n4']
+		}
+	});
+}
+
+// lazyLoad Images
+function lazyLoad() {
+	if ('loading' in HTMLImageElement.prototype) {
+		const images = document.querySelectorAll('img.lazyload');
+		images.forEach(function (img) {
+			img.src = img.dataset.src;
+			img.onload = function () {
+				img.classList.add('lazyloaded');
+			};
+			if (img.classList.contains('svg-html')) {
+				replaseInlineSvg(img);
+			}
+			if (img.classList.contains('lazyload-bg')) {
+				img.style.display = "none";
+				img.parentNode.style.backgroundImage = "url(" + img.dataset.src + ")";
+			}
+		});
+	} else {
+		if (!loadLazyLoadScript) {
+			loadLazyLoadScript = true;
+			const script = document.createElement("script");
+			script.async = true;
+			script.src = 'js/lazysizes.min.js';
+			document.body.appendChild(script);
+		}
+		document.addEventListener('lazyloaded', function (e) {
+			const img = e.target;
+			if (img.classList.contains('lazyload-bg')) {
+				img.style.display = 'none';
+				img.parentNode.style.backgroundImage = 'url(' + img.dataset.src + ')';
+			}
+			if (img.classList.contains('svg-html')) {
+				replaseInlineSvg(img);
+			}
+		});
+	}
+}
+
+// replaseInlineSvg
+function replaseInlineSvg(el) {
+	const imgID = el.getAttribute('id');
+	const imgClass = el.getAttribute('class');
+	const imgURL = el.getAttribute('src');
+
+	fetch(imgURL)
+		.then(data => data.text())
+		.then(response => {
+			const parser = new DOMParser();
+			const xmlDoc = parser.parseFromString(response, 'text/html');
+			const svg = xmlDoc.querySelector('svg');
+
+			if (typeof imgID !== 'undefined') {
+				svg.setAttribute('id', imgID);
+			}
+
+			if (typeof imgClass !== 'undefined') {
+				svg.setAttribute('class', imgClass + ' replaced-svg');
+			}
+
+			svg.removeAttribute('xmlns:a');
+
+			el.parentNode.replaceChild(svg, el);
+		});
+}
+
+// correctVh
+function correctVh() {
+	const vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', vh + 'px');
+}
+
+// mobile menu
+function mobileMenu() {
+	const openBtn = document.querySelector('.open-menu');
+
+	openBtn.addEventListener('click', function (event) {
+		event.preventDefault();
+		if (document.body.classList.contains('menu-opened')) {
+			document.body.classList.remove('menu-opened');
+		} else {
+			document.body.classList.add('menu-opened');
+		}
+	});
+};
+
+// jQuery
+(function ($) {
+	'use strict';
+
+	$(document).ready(function () {
+	}); // ready
+
+	$(window).on('resize', function () {
+	}); // resize
+
+	$(window).on('load', function () {
+	}); // load
+
+	// functions
+})(this.jQuery);
