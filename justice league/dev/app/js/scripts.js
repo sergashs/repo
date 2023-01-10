aa36c0851718333019388dce504cc08469edad5a
sergashs <sergas123456@gmail.com>
2023-01-10 17:13:13 +0200

    add new test task

---
+++ justice league/dev/app/js/scripts.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+	loadFonts();
+
+});
+
+// load fonts
+function loadFonts() {
+	WebFont.load({
+		google: {
+			families: ['Mulish:400,600,700', 'Marcellus:400']
+		}
+	});
+}
+
+
+
+
+
+
+
+
