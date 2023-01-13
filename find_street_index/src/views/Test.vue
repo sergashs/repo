865b48d57d0e7e0637f8da1491e8cfdf66dbef02
sergashs <sergas123456@gmail.com>
2023-01-13 18:14:18 +0200

    add function to sorted array

--- find_street_index/src/views/Test.vue
+++ find_street_index/src/views/Test.vue
@@ -1,8 +1,18 @@
 <template>
-	<div class="holder">
-		<button @click="minus">-</button>
-		<div>{{ date }}</div>
-		<button @click="plus">+</button>
+	<div>
+		{{ sortedArray.length }}
+		sortedArray : {{ sortedArray }} <br />
+		{{ allottedArray }} <br />
+		{{ activeSortedIndex }}
+
+		<button @click="setSortedArray('prev')">prev</button>
+		<button @click="setSortedArray('next')">next</button>
+
+		<div class="holder">
+			<button @click="minus">-</button>
+			<div>{{ date }}</div>
+			<button @click="plus">+</button>
+		</div>
 	</div>
 </template>
 
@@ -10,6 +20,9 @@
 export default {
 	data() {
 		return {
+			sortedArray: [],
+			activeSortedIndex: 0,
+			allottedArray: [],
 			dates: ["2021-12-01", "2021-12-02", "2021-12-03", "2021-12-04", "2021-12-05", "2021-12-06", "2021-12-07", "2021-12-08", "2021-12-09", "2021-12-10", "2021-12-11", "2021-12-12", "2021-12-13"],
 			filteredDates: [],
 			date: {
@@ -40,7 +53,36 @@ export default {
 			}
 
 			this.date.month--;
+		},
+		sortArray(arr) {
+			let numItems = 5;
+			this.sortedArray = arr.reduce((acc, curr, i) => {
+				if (!(i % numItems)) {
+					acc.push(arr.slice(i, i + numItems));
+				}
+				return acc;
+			}, []);
+		},
+		setSortedArray(direction) {
+			const arrLength = this.sortedArray.length - 1;
+
+			if (direction == "next") {
+				if (this.activeSortedIndex < arrLength) {
+					this.activeSortedIndex++;
+				}
+			}
+
+			if (direction == "prev") {
+				if (!this.activeSortedIndex <= 0) {
+					this.activeSortedIndex--;
+				}
+			}
+
+			this.allottedArray = this.sortedArray[this.activeSortedIndex];
 		}
+	},
+	mounted() {
+		this.sortArray(this.dates);
 	}
 };
 </script>
