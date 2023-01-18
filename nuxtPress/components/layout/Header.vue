0907d8d2182ccb48f8a2c4651959f66742d12772
sergashs <sergas123456@gmail.com>
2023-01-18 22:47:04 +0200

    update nuxtpress

---
+++ nuxtPress/components/layout/Header.vue
@@ -0,0 +1,47 @@
+<template>
+	<header class="header">
+		<div class="container">
+			<nav class="nav">
+				<ul class="menu">
+					<li v-for="(link, index) in menu" :key="index" class="menu-item">
+						<nuxt-link :to="link.url">
+							{{ link.title }}
+						</nuxt-link>
+					</li>
+				</ul>
+			</nav>
+		</div>
+		<Button label="plus" @click="counter++" />
+	</header>
+</template>
+
+<script setup>
+const menu = ref([
+	{
+		title: "Home",
+		url: "/"
+	},
+	{
+		title: "Posts",
+		url: "/posts"
+	},
+	{
+		title: "About",
+		url: "/about"
+	}
+]);
+</script>
+
+<style lang="scss" scoped>
+.header {
+	.menu {
+		display: flex;
+
+		.menu-item {
+			&:not(:last-child) {
+				margin-right: 20px;
+			}
+		}
+	}
+}
+</style>
--- nuxtPress/layouts/default.vue
+++ nuxtPress/layouts/default.vue
@@ -1,6 +1,11 @@
 <template>
 	<div>
+		<Header />
 		Some default layout shared across all pages <br />
 		<slot />
 	</div>
 </template>
+
+<script setup>
+import Header from "@/components/layout/Header";
+</script>
