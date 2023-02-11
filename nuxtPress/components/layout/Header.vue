efb198a9e84f0ac184ff3dae493d323bbeb1102b
sergashs <sergas123456@gmail.com>
2023-02-11 11:47:55 +0200

    ss

--- nuxtPress/components/layout/Header.vue
+++ nuxtPress/components/layout/Header.vue
@@ -1,6 +1,7 @@
 <template>
 	<header class="header">
 		<div class="container">
+			<nuxt-link to="/" class="logo">LOGO</nuxt-link>
 			<nav class="nav">
 				<ul class="menu">
 					<li v-for="(link, index) in menu" :key="index" class="menu-item">
@@ -34,13 +35,38 @@ const menu = ref([
 
 <style lang="scss" scoped>
 .header {
+	padding: 20px 0;
+
+	.container {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+
+	.logo {
+		font-weight: 600;
+		font-size: 20px;
+		text-transform: uppercase;
+	}
+
 	.menu {
+		list-style: none;
+		margin: 0;
+		padding: 0;
 		display: flex;
 
 		.menu-item {
 			&:not(:last-child) {
 				margin-right: 20px;
 			}
+		}
+	}
+
+	a {
+		text-decoration: none;
+
+		&:hover {
+			text-decoration: underline;
 		}
 	}
 }
