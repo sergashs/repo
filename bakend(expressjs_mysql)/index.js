01e1a047a32e94c6013233902f8ca473707b7759
sergashs <sergas123456@gmail.com>
2023-02-11 11:47:39 +0200

    ss

--- bakend(expressjs_mysql)/index.js
+++ bakend(expressjs_mysql)/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import postsRouter from "./src/Routes/posts.routes.js";
 import dataBase from "./src/database.js";
 
 const PORT = 5000;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/api', postsRouter)
 
