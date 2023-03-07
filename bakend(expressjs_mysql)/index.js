cec8d17c2f0c656bcc6a1d448e8b0694d0ba0cd5
sergashs <sergas123456@gmail.com>
2023-03-07 21:21:37 +0200

    add auth + middle for get accsess by token

--- bakend(expressjs_mysql)/index.js
+++ bakend(expressjs_mysql)/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import postsRouter from "./src/Routes/posts.routes.js";
+import authRouter from "./src/Routes/auth.routes.js";
 import dataBase from "./src/database.js";
 
 const PORT = 5000;
@@ -8,10 +9,15 @@ const PORT = 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/api', postsRouter)
+app.use('/api', postsRouter);
+app.use('/api', authRouter);
 
 
+// import auth from "./src/Middleware/auth.js";
 
+// app.post("/welcome", auth, (req, res) => {
+// 	res.status(200).send("Welcome 🙌 ");
+// });
 
 async function startApp() {
 	try {
