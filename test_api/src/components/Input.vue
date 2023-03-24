8a7864fbf4e1663a1bf74731862517aa40e1bfd3
sergashs <sergas123456@gmail.com>
2023-03-24 14:22:50 +0200

    updt

---
+++ test_api/src/components/Input.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="w-full">
+		<input type="text" :placeholder="input.placeholder" :value="modelValue" @input="onInput" class="h-10 rounded border p-2 text-sm w-full" />
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		input: {
+			type: Object,
+			default: () => ({})
+		},
+		modelValue: {
+			type: [String, Number],
+			default: ""
+		}
+	},
+	data() {
+		return {
+			value: ""
+		};
+	},
+	methods: {
+		onInput(event) {
+			this.$emit("update:modelValue", event.target.value);
+		}
+	}
+};
+</script>
