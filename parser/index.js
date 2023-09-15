73ab085279921848c5aeff0196ba0792f5dd90ee
sergashs <sergas123456@gmail.com>
2023-09-15 20:53:36 +0300

    updt

--- parser/index.js
+++ parser/index.js
@@ -3,82 +3,54 @@ import cheerio from 'cheerio';
 import fs from 'fs';
 import path from 'path';
 
-const base_url = "https://www.yakaboo.ua/ua/knigi.html?p=";
-const num_pages = 200;
-const image_folder = "images";
+const base_url = "https://www.bookovka.ua/uk/13-khudozhnya-literatura/s-1/82-ukrayinska?p=";
+const num_pages = 877;
 
-// Перевірка та створення папки для зображень
-if (!fs.existsSync(image_folder)) {
-	fs.mkdirSync(image_folder);
-}
+// Функція для отримання всіх елементів зі сторінки каталогу за їхнім тайтлом
+async function getItemsByTitle(title) {
+	const items = [];
 
-let existingBooks = [];
+	for (let page_number = 1; page_number <= num_pages; page_number++) {
+		try {
+			const url = `${base_url}${page_number}`;
+			console.log(`Запит на сторінку ${url}...`);
+			const response = await axios.get(url);
+			console.log(`Отримано сторінку ${url}. Розпарсювання...`);
+			const $ = cheerio.load(response.data);
 
-// Перевіряємо, чи є JSON-файл та читаємо дані про існуючі книжки
-if (fs.existsSync('books.json')) {
-	const jsonData = fs.readFileSync('books.json', 'utf-8');
-	existingBooks = JSON.parse(jsonData);
-}
-
-const books = [];
-
-async function fetchAndParsePage(page_number) {
-	try {
-		const url = `${base_url}${page_number}`;
-		console.log(`Запит на сторінку ${url}...`);
-		const response = await axios.get(url);
-		console.log(`Отримано сторінку ${url}. Розпарсювання...`);
-		const $ = cheerio.load(response.data);
-
-		$('.category-card.category-layout').each((index, element) => {
-			const author = $(element).find('.creators-label').text().trim();
-			const title = $(element).find('.ui-card-title.category-card__name').text().trim();
-			const img_url = $(element).find('.product-image.category-card-mode img').attr('src');
-
-			// Перевіряємо, чи книжка вже існує
-			const isExistingBook = existingBooks.some(existingBook => existingBook.title === title);
-
-			if (!isExistingBook) {
-				console.log(`Знайдено нову книжку: ${title} - ${author}`);
-				// Завантажуємо зображення
-				const img_name = path.join(image_folder, `${title}.jpg`);
-				axios({
-					url: img_url,
-					responseType: 'stream'
-				}).then(response => {
-					response.data.pipe(fs.createWriteStream(img_name));
-				});
-
-				const book = {
-					title: title,
-					author: author,
-					img: img_name
-				};
-
-				books.push(book);
-			} else {
-				console.log(`Книжка вже існує: ${title}`);
-			}
-		});
-	} catch (error) {
-		console.error(`Помилка при обробці сторінки ${page_number}: ${error.message}`);
+			$('.product__name a').each((index, element) => {
+				const itemTitle = $(element).text().trim();
+				if (itemTitle === title) {
+					const itemLink = $(element).attr('href');
+					items.push({
+						title: itemTitle,
+						link: itemLink
+					});
+				}
+			});
+		} catch (error) {
+			console.error(`Помилка при обробці сторінки ${page_number}: ${error.message}`);
+		}
 	}
+
+	return items;
 }
+
+// Виклик функції зі значенням тайтлу, який вас цікавить
+const titleToSearch = "Назва книги"; // Замініть це на тайтл, який вас цікавить
 
 async function main() {
-	console.log("Початок парсингу...");
-	for (let page_number = 1; page_number <= num_pages; page_number++) {
-		await fetchAndParsePage(page_number);
+	console.log(`Пошук елементів з тайтлом "${titleToSearch}"...`);
+	const itemsWithSameTitle = await getItemsByTitle(titleToSearch);
+
+	if (itemsWithSameTitle.length > 0) {
+		console.log(`Знайдено елементи з тайтлом "${titleToSearch}":`);
+		itemsWithSameTitle.forEach(item => {
+			console.log(item.link); // URL елемента
+		});
+	} else {
+		console.log(`Елементів з тайтлом "${titleToSearch}" не знайдено.`);
 	}
-
-	// Об'єднуємо існуючі та нові книжки
-	const allBooks = existingBooks.concat(books);
-
-	// Зберігаємо всі дані у JSON-файл
-	const json_data = JSON.stringify(allBooks, null, 4);
-	fs.writeFileSync('books.json', json_data, 'utf-8');
-
-	console.log("Завершено! Дані збережено у файлі 'books.json'.");
 }
 
 main();
