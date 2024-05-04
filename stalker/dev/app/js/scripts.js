cd0419506d22e645d833bb2c4d9c40bc07716913
sergashs <sergas123456@gmail.com>
2024-05-04 18:23:40 +0300

    delete trash

--- stalker/dev/app/js/scripts.js
+++ stalker/dev/app/js/scripts.js
@@ -244,19 +244,7 @@ function initSlides() {
 // fancyboxImges
 function fancyboxImges() {
 	const images = document.querySelectorAll('.single-post .wp-block-image.size-full');
-	const imagesLarge = document.querySelectorAll('.single-post .wp-block-image.size-large')
-	// const mainImg = document.querySelector('.single-post .img-holder img');
-
-	// if (mainImg) {
-	// 	const src = mainImg.getAttribute('src');
-	// 	const link = document.createElement('a');
-	// 	link.setAttribute('href', src);
-	// 	link.setAttribute('data-fancybox', 'wp-gallery');
-	// 	link.setAttribute('data-src', src);
-	// 	link.style.cursor = 'zoom-in';
-	// 	mainImg.parentNode.insertBefore(link, mainImg);
-	// 	link.appendChild(mainImg);
-	// }
+	const imagesLarge = document.querySelectorAll('.single-post .wp-block-image.size-large');
 
 	if (images) {
 		images.forEach(function (item) {
@@ -339,23 +327,3 @@ function headerCustomize() {
 	}
 }
 
-
-// (function () {
-// 	// Add event listener
-// 	document.addEventListener("mousemove", parallax);
-// 	const elem = document.querySelector("#parallax");
-// 	// Magic happens here
-// 	function parallax(e) {
-// 		let _w = window.innerWidth / 2;
-// 		let _h = window.innerHeight / 2;
-// 		let _mouseX = e.clientX;
-// 		let _mouseY = e.clientY;
-// 		let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
-// 		let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
-// 		let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
-// 		let x = `${_depth3}, ${_depth2}, ${_depth1}`;
-// 		console.log(x);
-// 		elem.style.backgroundPosition = x;
-// 	}
-
-// })();
